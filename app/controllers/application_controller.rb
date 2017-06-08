fd9f4627b41dcbfa4fc41f82e0cc24d972622f42
Nicolas Urrutia <nurrutialeuno@gmail.com>
2017-06-07 23:48:52 -0400

    Hasta el ultmo ejercicio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  
+  
   protect_from_forgery with: :exception
+
+  before_action :get_data
+
+  def get_data
+  	@users = ["Alumno1","Alumno2"]
+  end
+
 end
--- app/controllers/pags_controller.rb
+++ app/controllers/pags_controller.rb
@@ -1,7 +1,13 @@
 class PagsController < ApplicationController
+  
   def pag1
+  	render layout: "landing"
   end
 
   def pag2
   end
+
+  def pag3
+  end
+
 end
